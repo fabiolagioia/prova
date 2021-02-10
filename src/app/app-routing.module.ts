39d0d2afd5b3540fc493bba572de05f8a70c5523
Fabio La Gioia <flagioia@cybsec.it>
2021-02-10 15:17:58 +0100

    Inserimento slides e creazione struttura app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+ {
+   path: '',
+   loadChildren: () => import('./index/index.module').then(m => m.IndexPageModule)
+ },
+
+ {
+   path: '',
+   loadChildren: () => import ('./home/home.module').then (m=> m.HomePageModule)
+ }
 ];
 @NgModule({
   imports: [
