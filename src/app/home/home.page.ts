ee8cb6e208ce8e5b9123f9902373ba13f126e190
Fabio La Gioia <flagioia@cybsec.it>
2021-02-12 09:35:48 +0100

    Aggiornamento notifiche e aggiunta fake login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController,
+              private toastController: ToastController
+    ) { }
 
   ngOnInit() {
+    this.presentToast();
+    /* Per settare feed come tab iniziale */
+    this.navCtrl.navigateRoot('home/feed');
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Login succesfull',
+      duration: 3000,
+      color: 'primary'
+    });
+    toast.present();
   }
 
 }
